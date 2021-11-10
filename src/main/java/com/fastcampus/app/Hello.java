1ebbb27a2191947fb568b9f1fbfc58c02c4389e8
hongchii <flyh0321@gmail.com>
2021-11-10 18:33:30 +0900

    commit

---
+++ src/main/java/com/fastcampus/app/Hello.java
@@ -0,0 +1,14 @@
+package com.fastcampus.app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Hello {
+	
+	@RequestMapping("/hello")
+	public void main(){
+		System.out.println("Hello");
+	}
+
+}
